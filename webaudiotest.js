3100f966b80e968a8ab6207d39e93a415983ec07
nickboyko <nab9488@nyu.edu>
2024-04-06 18:46:23 -0400

    plotting test, porting random_walk function from python

--- webaudiotest.js
+++ webaudiotest.js
@@ -1,3 +1,20 @@
+function random_walk_boundary(steps, limit) {
+    let y = [];
+    let x_cur = 0;
+    const step_size = 1;
+
+    for (let i = 0; i < steps; i++) {
+        if (Math.abs(x_cur) + step_size > limit) {
+            const step = Math.sign(x_cur) * (-1 * step_size);
+        } else {
+            const step = (Math.round(Math.random()) * 2 - 1) * step_size;
+        }
+        x_cur += step;
+        y.push(step);
+    }
+    const x = y.map((sum = 0, n => sum += n));
+}
+
 document.querySelector('#play').addEventListener('click', () => {
     const actx = new (AudioContext || webkitAudioContext)();
     if (!actx) throw 'Not supported :(';
@@ -25,3 +42,13 @@ document.querySelector('#play').addEventListener('click', () => {
     // osc.start();
     // osc.stop(actx.currentTime + 2);
 });
+
+TESTER = document.getElementById('tester');
+
+Plotly.plot( TESTER, [{
+    x: [1, 2, 3, 4, 5],
+    y: [1, 2, 4, 8, 16] }], { 
+    margin: { t: 0 } }, {showSendToCloud:true} );
+
+/* Current Plotly.js version */
+console.log( Plotly.BUILD );
