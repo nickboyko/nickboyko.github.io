e49d04b6ff1d384fcf0a1787add1231128aba620
nickboyko <nab9488@nyu.edu>
2024-04-06 19:11:15 -0400

    test

--- webaudiotest.js
+++ webaudiotest.js
@@ -1,19 +1,19 @@
-function random_walk_boundary(steps, limit) {
-    let y = [];
-    let x_cur = 0;
-    const step_size = 1;
+// function random_walk_boundary(steps, limit) {
+//     let y = [];
+//     let x_cur = 0;
+//     const step_size = 1;
 
-    for (let i = 0; i < steps; i++) {
-        if (Math.abs(x_cur) + step_size > limit) {
-            const step = Math.sign(x_cur) * (-1 * step_size);
-        } else {
-            const step = (Math.round(Math.random()) * 2 - 1) * step_size;
-        }
-        x_cur += step;
-        y.push(step);
-    }
-    const x = y.map((sum = 0, n => sum += n));
-}
+//     for (let i = 0; i < steps; i++) {
+//         if (Math.abs(x_cur) + step_size > limit) {
+//             const step = Math.sign(x_cur) * (-1 * step_size);
+//         } else {
+//             const step = (Math.round(Math.random()) * 2 - 1) * step_size;
+//         }
+//         x_cur += step;
+//         y.push(step);
+//     }
+//     const x = y.map((sum = 0, n => sum += n));
+// }
 
 document.querySelector('#play').addEventListener('click', () => {
     const actx = new (AudioContext || webkitAudioContext)();
